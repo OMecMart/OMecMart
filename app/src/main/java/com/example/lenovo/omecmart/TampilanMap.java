1c283703d56e6dd3e7788bcddd855853990334ec
3brothers\Rahman <rahmanpurwanto1996@gmail.com>
2017-05-16 15:40:51 +0700

    iki rek

---
+++ app/src/main/java/com/example/lenovo/omecmart/TampilanMap.java
@@ -0,0 +1,13 @@
+package com.example.lenovo.omecmart;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TampilanMap extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tampilan_map);
+    }
+}
