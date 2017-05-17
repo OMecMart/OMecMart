4df43223b44065e5044c888ab9a2f1d8ba4e5a5f
user-PC\user <ilhammuhammad9945@gmail.com>
2017-05-17 15:16:38 +0700

    Commit

---
+++ app/src/main/java/com/example/lenovo/omecmart/Constants.java
@@ -0,0 +1,13 @@
+package com.example.lenovo.omecmart;
+
+/**
+ * Created by user on 11/05/2017.
+ */
+
+public class Constants {
+
+    private static final String ROOT_URL = "http://192.168.43.192/Omecmart/v1/";
+
+    public static final String URL_REGISTER = ROOT_URL+"registerUser.php";
+   // public static final String URL_LOGIN = ROOT_URL+"userLogin.php";
+}
