6f0f05392cedeeac0ea627cdf0616e76fbe798a0
Khendy10 <khendynatural10@gmail.com>
2017-05-24 11:21:08 +0700

    Pop Up

--- app/src/main/java/com/example/lenovo/omecmart/Daftar.java
+++ app/src/main/java/com/example/lenovo/omecmart/Daftar.java
@@ -1,7 +1,7 @@
 package com.example.lenovo.omecmart;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class Daftar extends AppCompatActivity {
 
@@ -11,3 +11,4 @@ public class Daftar extends AppCompatActivity {
         setContentView(R.layout.activity_daftar);
     }
 }
+
--- app/src/main/java/com/example/lenovo/omecmart/MainActivity.java
+++ app/src/main/java/com/example/lenovo/omecmart/MainActivity.java
@@ -1,13 +1,52 @@
 package com.example.lenovo.omecmart;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-
-public class MainActivity extends AppCompatActivity {
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.PopupMenu;
+import android.widget.Toast;
+import android.widget.TextView;
+public class MainActivity extends ActionBarActivity implements
+        PopupMenu.OnMenuItemClickListener{
+    TextView teksMenu;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState); setContentView(R.layout.activity_main);
+    teksMenu= (TextView)findViewById(R.id.teksMenu);
+        Button btnPopUp = (Button)findViewById(R.id.btnPopUp);
+        btnPopUp.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                PopupMenu menuPopUp = new PopupMenu(MainActivity.this, v);
+                menuPopUp.setOnMenuItemClickListener(MainActivity.this);
+                menuPopUp.inflate(R.menu.menu_main);
+                menuPopUp.show();
+            }
+        });
+    }
+    public boolean onMenuItemClick(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.menuSatu:
+                teksMenu.setText("Ini menu satu");
+                return true;
+            case R.id.menuDua:
+                teksMenu.setText("Ini menu Dua");
+                return true;
+            case R.id.shareItem:
+                Intent share = new Intent(Intent.ACTION_SEND);
+                share.setType("text/plain");
+                share.putExtra(Intent.EXTRA_SUBJECT, "Ini Judul Share");
+                share.putExtra(Intent.EXTRA_TEXT, "http://www.imastudio.co.id");
+                startActivity(Intent.createChooser(share, "Share Link!"));
+        }
+        return true
+                ;
     }
 }
